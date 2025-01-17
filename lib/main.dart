2df8af8627c8d3a6eadfef03b0d527dbaa22ac9b
ahmadrahimifar0310 <ahmadrf0208@gmail.com>
2025-01-17 04:13:57 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
-
+ 
 class _MyAppState extends State<MyApp> {
   ThemeMode themeMode = ThemeMode.dark;
 
